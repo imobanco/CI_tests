ab3d1b5c6812fea98f469866a763f7c570b54bb0
Pedro Regis <pedroalencarregis@hotmail.com>
2020-08-22 17:25:55 -0300

    Mais engenharia

--- create_ci_env.sh
+++ create_ci_env.sh
@@ -23,7 +23,7 @@ ENV_VARIABLES=$(grep '\S' "$ENV_SAMPLE" | grep --invert-match '^#' | cut --delim
 
 for env_variable_name in $(echo "$ENV_VARIABLES"); do
     ci_env_variable_name=secrets.CI_"$env_variable_name"
-    echo $env_variable_name=${{ "$(ci_env_variable_name)" }} >> "$ENV_OUTPUT_FILE_NAME"
+    echo $env_variable_name='${{ '"$ci_env_variable_name"' }}' >> "$ENV_OUTPUT_FILE_NAME"
     # echo secrets."$ci_env_variable_name"
 done
 
