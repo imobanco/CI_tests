546ee0b33b94a7da3efa79d4aa2fc011b5074c8d
Pedro Regis <pedroalencarregis@hotmail.com>
2020-08-24 17:41:51 -0300

    Mais teste

--- create_ci_env.sh
+++ create_ci_env.sh
@@ -23,8 +23,12 @@ ENV_VARIABLES=$(grep '\S' "$ENV_SAMPLE" | grep --invert-match '^#' | cut --delim
 
 for env_variable_name in $(echo "$ENV_VARIABLES"); do
     ci_env_variable_name=CI_"$env_variable_name"
-    echo -e $env_variable_name=${ci_env_variable_name} >> "$ENV_OUTPUT_FILE_NAME"
+    echo -e $env_variable_name=$ci_env_variable_name >> "$ENV_OUTPUT_FILE_NAME"
 done
+
+echo -e "Testando!" >> "$ENV_OUTPUT_FILE_NAME"
+
+echo -e $CI_SECRET_KEY >> "$ENV_OUTPUT_FILE_NAME"
 
 echo '---> BEGIN: cat "$ENV_OUTPUT_FILE_NAME"'
 cat "$ENV_OUTPUT_FILE_NAME"
