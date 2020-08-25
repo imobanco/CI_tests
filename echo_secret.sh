807f4af32ee0af57fde187070eda14f7ffae3b0a
Pedro Regis <pedroalencarregis@hotmail.com>
2020-08-25 16:52:47 -0300

    Remove comentários e arquivos não utilizados mais

--- echo_secret.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-touch .env.ci
-echo -e SECRET_KEY=${{ secrets.CI_SECRET_KEY }} >> .env.ci
-echo -e OUTRO=${{ secrets.CI_OUTRO }} >> .env.ci
-exit 0
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-
-echo CI_SECRET_KEY=$CI_SECRET_KEY > file.txt
-
-cat file.txt
-
